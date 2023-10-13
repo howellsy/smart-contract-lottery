2b691508fb595bffa06d5833920e6e745b2eaac5
howellsy <jamie.howells@gmail.com>
2023-10-13 11:16:14 +0100

    Initial unit test set up for the Raffle contract

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -125,4 +125,8 @@ contract Raffle is VRFConsumerBaseV2 {
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
+
+    function getRaffleState() external view returns (RaffleState) {
+        return s_raffleState;
+    }
 }
