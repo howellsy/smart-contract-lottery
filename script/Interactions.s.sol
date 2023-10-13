ccd777de0f3634f140b4083ccaa958b8cc5ee2c3
howellsy <jamie.howells@gmail.com>
2023-10-13 13:57:14 +0100

    Helper to create VRF coordinator subscriptions

---
+++ script/Interactions.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+
+contract CreateSubscription is Script {
+    function createSubscriptionUsingConfig() public returns (uint64) {
+        HelperConfig helperConfig = new HelperConfig();
+        (,, address vrfCoordinator,,,) = helperConfig.activeNetworkConfig();
+        return createSubscription(vrfCoordinator);
+    }
+
+    function createSubscription(address vrfCoordinator) public returns (uint64) {
+        console.log("Creating subscription on ChainId: %s", block.chainid);
+
+        vm.startBroadcast();
+        uint64 subId = VRFCoordinatorV2Mock(vrfCoordinator).createSubscription();
+        vm.stopBroadcast();
+
+        console.log("Created subscription with id: %s", subId);
+        console.log("Please update subscriptionId in HelperConfig.s.sol");
+
+        return subId;
+    }
+
+    function run() external returns (uint64) {
+        return createSubscriptionUsingConfig();
+    }
+}
